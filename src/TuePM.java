7f7d630632c24a1e7550bde9526c20c08b7119c2
general <general@svede.local>
2014-06-25 12:19:22 -0400

    made some changes

--- src/TuePM.java
+++ src/TuePM.java
@@ -1,5 +1,25 @@
 public class TuePM {
-	public static void main(String[] arguments){
+
+	public static String[][] map;
+	public static int myXCoord;
+	public static int myYCoord;
+
+	public static void main(String[] arguments) {
 		System.out.println("hello world");
+		map = new String[10][10];
+		myXCoord = 5;
+		myYCoord = 5;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				map[i][j] = "";
+			}
+		}
+		map[5][5] = "forest";
+		System.out.println(showMyLocation(myXCoord, myYCoord, map));
 	}
+
+	public static String showMyLocation(int a, int b, String[][] myMap) {
+		return myMap[a][b];
+	}
+
 }
