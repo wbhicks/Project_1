a5aaa2d63f67d5640f99ea5a953faa5f35bee738
wbhicks <wbhicks@users.noreply.github.com>
2014-06-28 23:36:02 -0400

    Created LoadFile.java

--- src/LoadFile.java
+++ src/LoadFile.java
@@ -1,5 +1,10 @@
 public class LoadFile {
 
-
-
+	public static go() {
+		BufferedReader reader = new BufferedReader(new FileReader("test_map.txt"));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			System.out.println("(" + line + ")")
+		}
+	}
 }
